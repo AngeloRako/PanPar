705412227952dd91fc770ae16382ed3e2318a44b
Raffoman <raffma1996@gmail.com>
2020-07-05 22:00:20 +0200

    Trasformata la ListaPanieriActivity.kt nel fragment ListaPanieriFragment.kt Aggiunte immagine vuota da mostrare in assenza di immagini e fragment_lista_panieri.xml che ora contiene la recycler_view Modificato il nav_graph.xml, ora non ha più ListaPanieriActivity.kt come home Modificata activity_lista_panieri.xml per ospitare il fragment e la navbar

--- app/src/main/java/com/rapnap/panpar/view/ListaPanieriActivity.kt
+++ app/src/main/java/com/rapnap/panpar/view/ListaPanieriActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rapnap.panpar.R
@@ -17,24 +22,21 @@ import kotlinx.android.synthetic.main.activity_lista_panieri.*
 
 class ListaPanieriActivity : AppCompatActivity() {
 
-    private lateinit var linearLayoutManager: LinearLayoutManager
-    private lateinit var adapter: RecyclerAdapter
-    private var panieriList : ArrayList<Paniere> = ArrayList()
-
-    private val listaPanieriVM : ListaPanieriViewModel by viewModels()
-
     //private val lastVisibleItemPosition: Int
     //    get() = linearLayoutManager.findLastVisibleItemPosition()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme_NoActionBar)
         setContentView(R.layout.activity_lista_panieri)
 
-        linearLayoutManager = LinearLayoutManager(this)
-        recycler_view.layoutManager = linearLayoutManager
-
-        adapter = RecyclerAdapter(panieriList)
-        recycler_view.adapter = adapter
+        //Imposto la toolbar dell'activity
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.lista_panieri_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val toolbar = findViewById<Toolbar>(R.id.topAppBar)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
 
         //setRecyclerViewScrollListener()
     }
@@ -42,41 +44,6 @@ class ListaPanieriActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        //Parte chiamando la funzione che comunica con la repository per prendere i panieri
-        //in base al punteggio dell'utente
-        listaPanieriVM.getPanieriByScore() {
-
-            Log.d("ACTIVITY", "Ho chiamato getPanieriByScore")
-
-            //Osserva i panieri ottenuti
-            listaPanieriVM.getListaPanieri().observe(this, Observer<ArrayList<Paniere>> {
-                //Assegna a panieriList i panieri ottenuti, che verrà poi passato all'adapter
-                panieriList = it
-                Log.d("ACTIVITY", "Ho assegnato ad i panieri i valori che stavano nel DB." +
-                        " La dimensione della lista dei panieri è: " + panieriList.size.toString())
-
-                panieriList.forEach() {
-                    runOnUiThread {             //Fondamentalmente perché sì, funziona anche senza
-                        adapter.addNewItem(it)
-                        adapter.notifyItemInserted((panieriList.size-1))    //Posizione in cui ho inserito, sempre alla fine
-                        Log.d("ACTIVITY", "Ho " + adapter.itemCount.toString() + " panieri.")
-                    }
-                }
-            })
-        }
     }
 
-    //Questo aggiorna la view quando scrolli, ma se carichiamo tutti i panieri una volta e per tutte
-    //non serve (magari potrebbe essere un miglioramento futuro, tipo caricarne prima 10 poi gli
-    //altri man mano che scrolli)
-
-//    private fun setRecyclerViewScrollListener() {
-//        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-//            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-//                super.onScrollStateChanged(recyclerView, newState)
-//                val totalItemCount = recyclerView.layoutManager!!.itemCount
-//                richiediPaniere()
-//            }
-//        })
-//    }
 }
